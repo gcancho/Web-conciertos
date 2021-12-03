c5d7f0dafb0841a92f12657e2787208c142571a8
gcancho <giovannicb19@gmail.com>
2021-12-03 12:24:38 -0500

    creando password hasheados

---
+++ admin/insertar-admin.php
@@ -0,0 +1,17 @@
+<?php
+
+if(isset($_POST['agregar-admin'])){
+    $usuario = $_POST['usuario'];
+    $nombre = $_POST['nombre'];
+    $password = $_POST['password'];
+
+    $opciones = array(
+        'cost' => 12
+    );
+
+    $password_hashed = password_hash($password, PASSWORD_BCRYPT, $opciones);
+
+    echo $password_hashed;
+}
+
+?>
